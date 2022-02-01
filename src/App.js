1da22be06f2ba9376390799920c98467b021369e
Carl <cfds2003@gmail.com>
2022-02-01 17:01:23 -0500

    Sobre nosotros añadido

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import Header from './components/header';
 import Inicio from './components/Inicio';
 import Ofertas from './components/Ofertas';
+import SobreN from './components/SobreNosotros';
+
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <Inicio/>
       <Ofertas/>
+      <SobreN/>
     </div>
   );
 }
