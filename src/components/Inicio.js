53bf918b89dfdfdeb55eda587f5d2c3981af0c92
Carl <cfds2003@gmail.com>
2022-01-31 20:18:03 -0500

    imagenes agregadas

---
+++ src/components/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Inicio() {
+    return(
+        <div className="Inicio">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit nihil eaque, asperiores error optio ducimus consequuntur dolorem iste magni aliquam ipsum repudiandae ipsam? Est sint cupiditate tempore cum nulla.</p>
+        </div>
+    );
+}
+
+export default Inicio;
--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,17 @@ import React from 'react';
 function Header() {
     return(
         <div className='Header'>
-            <nav>
-                <a href="#">Inicio</a>
-                <a href="#">Ofertas</a>
-                <a href="#">Sobre nosotros</a>
-                <a href="#">Mas informacion</a>
-            </nav>
-            <h2>Lorem ipsum dolor sit</h2>
-            <p>Lorem ipsum</p>
+            <div className="content">
+                <nav>
+                    <a href="#">Inicio</a>
+                    <a href="#">Ofertas</a>
+                    <a href="#">Sobre nosotros</a>
+                    <a href="#">Mas informacion</a>
+                </nav>
+                <h2>Lorem ipsum dolor sit</h2>
+                <p>Lorem ipsum</p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,224L720,32L1440,192L1440,320L720,320L0,320Z"></path></svg>
         </div>
     );
 }
