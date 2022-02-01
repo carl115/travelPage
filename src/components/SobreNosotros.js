3177ead9fea716e19fb4b7ce1ce4ad5c92951075
Carl <cfds2003@gmail.com>
2022-02-01 17:04:50 -0500

    modicacion contenido

--- src/components/SobreNosotros.js
+++ src/components/SobreNosotros.js
@@ -3,11 +3,11 @@ import React from 'react';
 function SobreNosotros() {
     return(
         <div className="SobreNosotros">
-            <div className="content-1">
+            <div className="content">
                 <strong>Lorem ipsum dolor sit</strong>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad quidem saepe dignissimos debitis adipisci, dolor mollitia exercitationem expedita praesentium, obcaecati in enim amet magnam eaque voluptas. Dolor et fugiat dicta eum illum totam dolorem autem. Qui explicabo veritatis beatae.</p>
             </div>
-            <div className="content-2">
+            <div className="content">
                 <strong>Lorem ipsum dolor sit</strong>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad quidem saepe dignissimos debitis adipisci, dolor mollitia exercitationem expedita praesentium, obcaecati in enim amet magnam eaque voluptas. Dolor et fugiat dicta eum illum totam dolorem autem. Qui explicabo veritatis beatae.</p>
             </div>
