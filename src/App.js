b0f6b291463e824daace244389405725df44c392
Carl <cfds2003@gmail.com>
2022-02-01 14:42:07 -0500

    Inicio añadido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/header';
+import Inicio from './components/Inicio';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Inicio/>
     </div>
   );
 }
