02b66ed3d8d77e19d6bd7129152fb92e73ab8da2
Carl <cfds2003@gmail.com>
2022-01-31 19:48:58 -0500

    header agregado

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div className='Header'>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Ofertas</a>
+                <a href="#">Sobre nosotros</a>
+                <a href="#">Mas informacion</a>
+            </nav>
+            <h2>Lorem ipsum dolor sit</h2>
+            <p>Lorem ipsum</p>
+        </div>
+    );
+}
+
+export default Header;
