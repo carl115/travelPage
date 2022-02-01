e52ed5d33ae49324fd6d11f24338b0ff17a82f31
Carl <cfds2003@gmail.com>
2022-02-01 16:57:36 -0500

    Sobre Nosotros añadida

---
+++ src/components/SobreNosotros.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SobreNosotros() {
+    return(
+        <div className="SobreNosotros">
+            <div className="content-1">
+                <strong>Lorem ipsum dolor sit</strong>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad quidem saepe dignissimos debitis adipisci, dolor mollitia exercitationem expedita praesentium, obcaecati in enim amet magnam eaque voluptas. Dolor et fugiat dicta eum illum totam dolorem autem. Qui explicabo veritatis beatae.</p>
+            </div>
+            <div className="content-2">
+                <strong>Lorem ipsum dolor sit</strong>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad quidem saepe dignissimos debitis adipisci, dolor mollitia exercitationem expedita praesentium, obcaecati in enim amet magnam eaque voluptas. Dolor et fugiat dicta eum illum totam dolorem autem. Qui explicabo veritatis beatae.</p>
+            </div>
+        </div>
+    );
+}
+
+export default SobreNosotros;
