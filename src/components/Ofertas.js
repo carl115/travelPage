246344fe87e28f89c2a2de3479020f5af7461e7d
Carl <cfds2003@gmail.com>
2022-02-02 17:35:22 -0500

    Arreglo ofertas

--- src/components/Ofertas.js
+++ src/components/Ofertas.js
@@ -1,38 +1,11 @@
 import React from 'react';
 
-function Oferta() {
+function Oferta(props) {
     return(
-        <div className="Oferta">
-            <div className="product">
-                <img src={require('../img/pexels-julius-silver-753626.jpg')} alt="producto1" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
-            <div className="product">
-                <img src={require('../img/pexels-max-ravier-3331064.jpg')} alt="producto2" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
-            <div className="product">
-                <img src={require('../img/pexels-oliver-sjöström-1078983.jpg')} alt="producto3" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
-            <div className="product">
-                <img src={require('../img/pexels-oliver-sjöström-1433052.jpg')} alt="producto4" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
-            <div className="product">
-                <img src={require('../img/pexels-pixabay-237272.jpg')} alt="producto5" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
-            <div className="product">
-                <img src={require('../img/pexels-pixabay-38238.jpg')} alt="producto6" />
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
-            </div>
+        <div className="product">
+            <img src={require(props.img)} alt={props.textoImg} />
+            <p>Lorem ipsum dolor sit amet</p>
+            <p><strong>Noche </strong>$00.00</p>
         </div>
     );
 }
