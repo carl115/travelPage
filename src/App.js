e49ed70a26d99da4b7b535c6534bb95e324344bb
Carl <cfds2003@gmail.com>
2022-02-02 17:44:04 -0500

    objetos ofertas añadido

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
     <div className="App">
       <Header/>
       <Inicio/>
-      <Ofertas/>
+      <div className="Ofertas">
+        <Ofertas img="../src/img/pexels-julius-silver-753626.jpg" textImg="producto1"/>
+        <Ofertas img="../src/img/pexels-max-ravier-3331064.jpg" textImg="producto2"/>
+        <Ofertas img="../src/img/pexels-oliver-sjöström-1078983.jpg" textImg="producto3"/>
+        <Ofertas img="../src/img/pexels-oliver-sjöström-1433052.jpg" textImg="producto4"/>
+        <Ofertas img="../src/img/pexels-pixabay-237272.jpg" textImg="producto5"/>
+        <Ofertas img="../src/img/pexels-pixabay-38238.jpg" textImg="producto6"/>
+      </div>
       <SobreN/>
       <MasInfo/>
       <Footer/>
