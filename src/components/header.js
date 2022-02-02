19e999719a99531e5e490a2fe91b46cfe0babdb6
Carl <cfds2003@gmail.com>
2022-02-02 14:27:30 -0500

    Efecto de estilo añadido

--- src/components/header.js
+++ src/components/header.js
@@ -14,6 +14,9 @@ function Header() {
                 <h2>Lorem ipsum dolor sit</h2>
                 <p>Lorem ipsum</p>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#ffffff" fill-opacity="1" d="M0,288L720,32L1440,288L1440,320L720,320L0,320Z"></path>
+            </svg>
         </div>
     );
 }
