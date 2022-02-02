b31912d5e5d975c60ff2c10634f72b39ac314f02
Carl <cfds2003@gmail.com>
2022-02-02 16:18:34 -0500

    Modificacion del archivo

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles/Inicio.css';
 
 function Inicio() {
     return(
         <div className="Inicio">
             <div className="content-1">
-            <img src={require('../img/pexels-asad-photo-maldives-1450340.jpg')} alt="imagen de inicio"/>
+                <img src={require('../img/pexels-asad-photo-maldives-1450340.jpg')} alt="imagen de inicio"/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt natus, animi aspernatur eaque optio ratione cumque harum! Quibusdam voluptatum architecto ad saepe quisquam voluptatibus dignissimos quidem voluptate consectetur, molestias quos provident reiciendis corporis qui repellendus, harum beatae necessitatibus velit? Dolore?</p>
             </div>
             <div className="content-2">
