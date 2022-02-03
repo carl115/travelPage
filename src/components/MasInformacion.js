6ca543e838a15cf886e561f743abdff11b0cbd5f
Carl <cfds2003@gmail.com>
2022-02-03 16:04:11 -0500

    Estilos Mas Informacion agregados

--- src/components/MasInformacion.js
+++ src/components/MasInformacion.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './styles/MasInformacion.css';
 
 function MasInformacion() {
     return(
         <div className="MasInformacion">
-            <div className="content">
+            <div className="content-text">
                 <h2>Lorem ipsum dolor sit amet</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex maiores eos excepturi dolor suscipit placeat alias enim dolore magnam, accusantium ducimus iure natus a reiciendis adipisci ad quibusdam doloribus saepe?</p>
                 <br/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sunt nam excepturi. Officiis, voluptatem quo temporibus repellendus libero recusandae rem molestias blanditiis atque facere excepturi deserunt reiciendis accusamus, iste provident?</p>
             </div>
-            <div className="content">
+            <div className="content-img">
                 <img src={require('../img/pexels-asad-photo-maldives-457878.jpg')} alt="imagen1" />
                 <img src={require('../img/pexels-julius-silver-753626.jpg')} alt="imagen2" />
                 <img src={require('../img/pexels-oliver-sjöström-1078983.jpg')} alt="imagen3" />
