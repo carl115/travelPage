47d6ad3565b8770501a9912c048aaf6844d794d2
Carl <cfds2003@gmail.com>
2022-04-13 20:08:42 -0500

    Modificacion del header

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,19 @@ function Header() {
     return(
         <div className='Header'>
             <nav>
-                <a href="#">Inicio</a>
-                <a href="#">Ofertas</a>
-                <a href="#">Sobre nosotros</a>
-                <a href="#">Mas informacion</a>
+                <ion-icon name="airplane"></ion-icon>
+                <div className="links">
+                    <a href="#">Inicio</a>
+                    <a href="#">Ofertas</a>
+                    <a href="#">Sobre nosotros</a>
+                    <a href="#">Mas informacion</a>
+                </div>
             </nav>
+            <div className="img-header">
+            </div>
             <div className="content">
                 <h2>Lorem ipsum dolor sit</h2>
-                <p>Lorem ipsum</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, obcaecati!</p>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#ffffff" fill-opacity="1" d="M0,288L720,32L1440,288L1440,320L720,320L0,320Z"></path>
