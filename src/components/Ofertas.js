2520a924ea9b717416d7951d70c3c9a3051d936a
Carl <cfds2003@gmail.com>
2022-04-13 20:55:46 -0500

    Cambios tarjetas

--- src/components/Ofertas.js
+++ src/components/Ofertas.js
@@ -5,9 +5,12 @@ function Oferta(props) {
     return(
         <div className="product">
             <img src={props.img} alt={props.nameImg} />
-            <div className="content-text">
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><strong>Noche </strong>$00.00</p>
+            <div className="content">
+                <strong>Description</strong>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, eaque!</p>
+                <br />
+                <p><strong>Night </strong>$00.00</p>
+                <button><strong>Reserve</strong></button>
             </div>
         </div>
     );
