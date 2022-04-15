f56b95cd7d3fdfb869c6dd975fb5ae18f6dcb568
Carl <cfds2003@gmail.com>
2022-04-14 19:05:45 -0500

    slider de comentarios agregado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Inicio from './components/Inicio';
 import Ofertas from './components/Ofertas';
 import Empleado from './components/Empleado';
 import MasInfo from './components/MasInformacion';
+import Comentario from './components/Comentario';
 import Footer from './components/Footer';
 
 function App() {
@@ -48,6 +49,18 @@ function App() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="waveBottom"><path fill="#ffffff" fill-opacity="1" d="M0,192L48,181.3C96,171,192,149,288,149.3C384,149,480,171,576,192C672,213,768,235,864,234.7C960,235,1056,213,1152,181.3C1248,149,1344,107,1392,85.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       </div>
       <MasInfo/>
+      <div className="Comentarios">
+        <button>
+          <ion-icon name="arrow-round-back"></ion-icon>
+        </button>
+        <div className="slider">
+          <Comentario className="comentario" estrella={"4.5"} />
+          <Comentario className="comentario" estrella={"2.5"} />
+        </div>
+        <button>
+          <ion-icon name="arrow-round-forward"></ion-icon>
+        </button>
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/components/Comentario.js
@@ -0,0 +1,145 @@
+import React from "react";
+import "./styles/Comentario.css";
+
+function Comentario(props) {
+  return (
+    <div className="Comentario">
+      <h2>Lorem ipsum dolor sit.</h2>
+      <div className="puntos">
+        {
+        (() => {
+          switch (props.estrella) {
+            case "0":
+              return (
+                <>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "0.5":
+              return (
+                <>
+                  <ion-icon name="star-half"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "1":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "1.5":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-half"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "2":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "2.5":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-half"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "3":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "3.5":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-half"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "4":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-outline"></ion-icon>
+                </>
+              );
+            break;
+            case "4.5":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star-half"></ion-icon>
+                </>
+              );
+            break;
+            case "5":
+              return (
+                <>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                  <ion-icon name="star"></ion-icon>
+                </>
+              );
+            break;
+            default: alert('No funciona');
+            break;
+          }
+
+        })()
+        }
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ratione tempore aperiam dolorum iure voluptates delectus similique blanditiis voluptatem rerum. Magnam consequuntur molestiae impedit asperiores vero natus distinctio quam repudiandae?</p>
+    </div>
+  );
+}
+
+export default Comentario;
